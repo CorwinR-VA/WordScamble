8504b9d5ce3013523a9e216aad5db6cc5d13be2e
Corwin Rainier <corwinrainier89@gmail.com>
2022-06-21 13:26:13 -0300

    Initial Commit

---
+++ WordScamble/WordScambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScambleApp.swift
+//  WordScamble
+//
+//  Created by Corwin Rainier on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
